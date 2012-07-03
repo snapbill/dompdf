9851549b1e28e969649cce8fe00c00525b7becaa
Max Rabkin <max.rabkin@gmail.com>
2012-07-03 13:57:38 +0200

    Don't include php-font-lib
    We only use standard fonts so we don't need php-font-lib to calculate metrics

--- dompdf/include/font_metrics.cls.php
+++ dompdf/include/font_metrics.cls.php
@@ -10,7 +10,7 @@
  */
 
 require_once DOMPDF_LIB_DIR . "/class.pdf.php";
-require_once DOMPDF_LIB_DIR . "/php-font-lib/classes/font.cls.php";
+// require_once DOMPDF_LIB_DIR . "/php-font-lib/classes/font.cls.php";
 
 /**
  * Name of the font cache file
